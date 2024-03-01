993d9a307e1a529b3ce8f25dd13b4780eb8e1615
Ben Lambert <bennett-spencer.lambert@pierer-innovation.com>
2024-03-01 09:38:30 +0100

    complete part of conversions

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let parts = s.split(",").collect::<Vec<&str>>();
+            println!("{:?}", parts);
+            if parts.len() < 2 {
+                Person::default()
+            } else if parts[0].len() == 0 {
+                Person::default()
+            } else {
+                let name = parts[0];
+                let age = parts[1].to_string();
+                let age_num = age.parse::<usize>();
+                match age_num {
+                    Ok(age) => Person {
+                        name: name.to_string(),
+                        age: age,
+                    },
+                    Err(_e) => Person::default(),
+                }
+            }
+        }
     }
 }
 
